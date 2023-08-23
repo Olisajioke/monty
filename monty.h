441ce6aa481242163631b5727cf66264c673fe73
olisajioke <chijioke914@mail.com>
2023-08-23 13:32:52 -0700

    Readline and Monty.h

---
+++ monty.h
@@ -0,0 +1,52 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+extern int arg;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void get_opcode_arg(stack_t **stack,
+char *current_line, unsigned int line_number);
+void get_arguments(stack_t **stack, char *opcode, unsigned int line_number);
+void process_opcode(stack_t **stack, char *argument,
+char *opcode, unsigned int line_number);
+void push_operation(stack_t **stack, unsigned int line_number);
+void pall_operation(stack_t **stack, unsigned int line_number);
+
+#endif /* _MONTY_H */
---
+++ readline.c
@@ -0,0 +1,102 @@
+#include "monty.h"
+
+/**
+ * get_opcode_arg - Parses the opcode and arguments from a line of input.
+ * @stack:    Pointer to the stack.
+ * @current_line: The current line of input.
+ * @line_number:   The current line number.
+ */
+void get_opcode_arg(stack_t **stack,
+char *current_line, unsigned int line_number)
+{
+	char *opcode = strtok(current_line, " \t\n");
+
+	if (opcode != NULL)
+	{
+		while (*opcode == ' ' || *opcode == '\t')
+			opcode++;
+		get_arguments(stack, opcode, line_number);
+	}
+}
+
+/**
+ * get_arguments - Parses the arguments from the opcode line.
+ * @stack:    Pointer to the stack.
+ * @opcode:   The opcode to be processed.
+ * @line_number:   The current line number.
+ */
+void get_arguments(stack_t **stack, char *opcode, unsigned int line_number)
+{
+	char *argument = strtok(NULL, " \t\n");
+
+	if (argument != NULL)
+	{
+		char *end = argument + strlen(argument) - 1;
+
+		while (*end == ' ' || *end == '\t' || *end == '\n')
+		{
+			*end = '\0';
+			end--;
+		}
+
+		process_opcode(stack, opcode, argument, line_number);
+	}
+	else
+	{
+		if (strcmp(opcode, "push") == 0)
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			process_opcode(stack, opcode, NULL, line_number);
+		}
+	}
+}
+
+/**
+ * process_opcode - Executes the appropriate operation based on opcode.
+ * @stack:    Pointer to the stack.
+ * @opcode:   The opcode to be processed.
+ * @argument: The argument associated with the opcode.
+ * @line_number:   The current line number.
+ */
+void process_opcode(stack_t **stack,
+char *opcode, char *argument, unsigned int line_number)
+{
+	instruction_t operations[] = {
+		{"push", push_operation},
+		{"pall", pall_operation}
+	};
+	size_t i = 0;
+	int found_opcode = 0;
+	size_t num_of_operations = sizeof(operations) / sizeof(instruction_t);
+
+	while (i < num_of_operations)
+	{
+		if (strcmp(opcode, operations[i].opcode) == 0)
+		{
+			if (strcmp(opcode, "push") == 0)
+			{
+				if (atoi(argument) != 0)
+					arg = atoi(argument);
+				else
+				{
+					fprintf(stderr, "L%u: usage: push integer\n", line_number);
+					exit(EXIT_FAILURE);
+				}
+			}
+			operations[i].f(stack, line_number);
+			i++;
+			found_opcode++;
+			break;
+		}
+		i++;
+	}
+	if (!found_opcode)
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
+}
